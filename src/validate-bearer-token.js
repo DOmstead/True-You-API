09703f5f3a2ef92dc9b95a66dfd0830ae5c41ba8
David Omstead <49734565+DOmstead@users.noreply.github.com>
2024-01-23 15:45:55 -0700

    Update validate-bearer-token.js

--- src/validate-bearer-token.js
+++ src/validate-bearer-token.js
@@ -6,6 +6,9 @@ const logger = require('./logger');
 //with a valid key can make requests to this API. 
 function validateBearerToken(req, res, next) {
   const authToken = req.get('Authorization');
+  console.log('Req.exports is = ' + req.exports);
+  console.log('Req.get is = ' + req.get);
+  console.log('Req.exports is = ' + req.authToken);
   
   if (!authToken || authToken.split(' ')[1] !== API_TOKEN) {
     logger.error(`Unauthorized request to path: ${req.path} with authToken *${authToken}*: Should be bearer *${API_TOKEN}* `);
