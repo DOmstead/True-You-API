e035327e6b4f901f828f1676a3c0e851f994775a
David Omstead <49734565+DOmstead@users.noreply.github.com>
2024-01-26 14:58:46 -0700

    Update validate-bearer-token.js

--- src/validate-bearer-token.js
+++ src/validate-bearer-token.js
@@ -5,15 +5,14 @@ const logger = require('./logger');
 //This function is a crucial step in this apps security. It ensures that only a program
 //with a valid key can make requests to this API. 
 function validateBearerToken(req, res, next) {
-  next();
-  // const authToken = req.get('Authorization');
-  // logger.error(`Testing request to path: ${req.path} with authToken ${authToken}: Should be bearer ${API_TOKEN} `);
+  const authToken = req.get('Authorization');
+  logger.error(`Testing request to path: ${req.path} with authToken ${authToken}: Should be bearer ${API_TOKEN} `);
   
-  // if (!authToken || authToken.split(' ')[1] !== API_TOKEN) {
-  //   logger.error(`Unauthorized request to path: ${req.path} with authToken *${authToken}*: Should be bearer *${API_TOKEN}* `);
-  //   return res.status(401).json({ error: 'Unauthorized request' });
-  // }
-  // next();
+  if (!authToken || authToken.split(' ')[1] !== API_TOKEN) {
+    logger.error(`Unauthorized request to path: ${req.path} with authToken *${authToken}*: Should be bearer *${API_TOKEN}* `);
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  next();
 }
 
 module.exports = validateBearerToken;
